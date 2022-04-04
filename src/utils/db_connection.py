0c1c14d8b7bced0460bd3cf2882d40e17f354ce9
Dani Mermelstein <dani.mermelstein@gmail.com>
2022-04-03 20:20:18 -0400

    fixing incorrect linting

--- src/utils/db_connection.py
+++ src/utils/db_connection.py
@@ -3,14 +3,15 @@ import pandas as pd
 import os
 
 
-def engine(
-    db_type="postgresql",
-    user_name=os.environ.get("DB_USER"),
-    password=os.environ.get("DB_PASSWORD"),
-    host=os.environ.get("DB_HOST"),
-    port=os.environ.get("DB_PORT"),
-    db_name=os.environ.get("DB_NAME"),
-):
+def engine(db_type, user_name, password, host, port, db_name):
+
+    db_type = "postgresql"
+    user_name = os.environ.get("DB_USER")
+    password = os.environ.get("DB_PASSWORD")
+    host = os.environ.get("DB_HOST")
+    port = os.environ.get("DB_PORT")
+    db_name = os.environ.get("DB_NAME")
+
     engine = create_engine(
         f"{db_type}://{user_name}:{password}@{host}:{port}/{db_name}"
     )
